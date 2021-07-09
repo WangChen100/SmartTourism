d7594d19cd015ff3bab15d5e37006c302f8c2d0a
wangc <wangchen100@163.com>
2021-07-09 22:52:10 +0800

    add function read_data()

---
+++ data.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+import os
+import pandas as pd
+
+
+
+
+def read_data(path):
+    '''
+    Read train/test data from corresponding CSV files
+    parameter:
+        path: floder path of dataset
+    return:
+        train: train data in DataFrame (pandas format)
+        test:  test data in DataFrame (pandas format)
+    '''
+    if not os.path.exists(path):
+        raise ValueError('This path is not existed!')
+    
+    train = pd.read_csv(path + 'train.csv')
+    test = pd.read_csv(path + 'test.csv')
+
+    print('Read dataset successfully!')
+
+    # print ('***********Train*************')
+    # print ('test')
+    # print (train.isnull().sum())
+    # print ('***********test*************')
+    # print (test.isnull().sum())
+
+    return train, test
+
+
+def count_data(train, test):
+    print ('***********Train*************')
+    print ('test')
+    print (train.isnull().sum())
+    print ('***********test*************')
+    print (test.isnull().sum())
+
+
+if __name__=='__main__':
+    # Here is an example to use function read_data()
+    path = 'dataset/titanic/'
+    data_trian, data_test = read_data(path)
+    count_data(data_trian, data_test)
---
+++ main.py
@@ -0,0 +1,9 @@
+# -*- coding: utf8 -*-
+
+from data import read_data
+
+# 读数据
+path = 'dataset/titanic/'
+data_train, data_test = read_data(path)
+
+# 模型
